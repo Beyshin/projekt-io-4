a5b94671e399e33890b2b3c6ed04500a841ea6d5
adamowskipl <matek0402@gmail.com>
2026-01-27 13:53:16 +0100

    guess a song changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import {SocketProvider} from "./context/SocketContext";
+import { SocketProvider } from "./context/SocketContext";
 import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
       <div className="App">
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
-                rel="stylesheet"/>
           <SocketProvider>
               <BrowserRouter>
-                  <AnimatedRoutes></AnimatedRoutes>
+                  <AnimatedRoutes />
               </BrowserRouter>
           </SocketProvider>
       </div>
